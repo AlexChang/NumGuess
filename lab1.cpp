4aad936a326f1ab27ef1e2f20ee93495b12d4bfd
AlexChang <zfmedisonxfmt@126.com>
2015-03-28 13:57:06 +0800

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,5 +1,5 @@
 // p1.cpp
-// F1403022班  5140309508  张福明  2015.03.25
+// F1403022°à  5140309508  ÕÅ¸£Ã÷  2015.03.25
 
 #include <iostream>
 #include <ctime>
@@ -15,51 +15,49 @@ int main()
 	
 	srand(time(NULL));								// initialization of the seed of random numbers
 	
-	cout << "需要玩游戏吗（Y/N）？ ";
+	cout << "ÐèÒªÍæÓÎÏ·Âð£¨Y/N£©£¿ ";
 	cin >> choose;
-	if (choose=='Y'){
-		do {
-			answer[0]=rand()*10/(RAND_MAX+1);		// generate random num1 
-			answer[1]=rand()*10/(RAND_MAX+1);		// generate random num2 
-			while (answer[1]==answer[0]){
-				answer[1]=rand()*10/(RAND_MAX+1);	// check if random num1 is the same as random num2 
-			}
-			answer[2]=rand()*10/(RAND_MAX+1);		// generate random num3
-			while (answer[2]==answer[1] || answer[2]==answer[0]){
-				answer[2]=rand()*10/(RAND_MAX+1);	// check if random num1 is the same as random num2 or num3 
-			}
+	while (choose=='Y'){
+		answer[0]=rand()*10/(RAND_MAX+1);		// generate random num1 
+		answer[1]=rand()*10/(RAND_MAX+1);		// generate random num2 
+		while (answer[1]==answer[0]){
+			answer[1]=rand()*10/(RAND_MAX+1);	// check if random num1 is the same as random num2 
+		}
+		answer[2]=rand()*10/(RAND_MAX+1);		// generate random num3
+		while (answer[2]==answer[1] || answer[2]==answer[0]){
+			answer[2]=rand()*10/(RAND_MAX+1);	// check if random num1 is the same as random num2 or num3 
+		}
 			
-			games+=1;								// record innings								
-			for (i=0; i<7; ++i){
-				cout << "请输入你猜测的数字:" << endl;
-				for (j=0; j<3; ++j)	cin >> guess[j];		// guess three numbers 
-				correctA=0;
-				correctB=0;
-				for (j=0; j<3; ++j){				// calculate A 
-					if (answer[j] == guess[j]) correctA+=1;		
-				}
-				for (j=0; j<3; ++j){				// calculate B
-					if (answer[j]==guess[(j+1)%3] || answer[j]==guess[(j+2)%3])	correctB+=1;	
-				}
-				if (correctA==3){
-					win+=1;							// record innings for the win 
-					cout << "恭喜，你猜对了";
-					break; 
-				}
-				else cout << correctA << 'A' << correctB << 'B' << endl;	// output A,B
+		games+=1;								// record innings								
+		for (i=0; i<7; ++i){
+			cout << "ÇëÊäÈëÄã²Â²âµÄÊý×Ö:" << endl;
+			for (j=0; j<3; ++j)	cin >> guess[j];		// guess three numbers 
+			correctA=0;
+			correctB=0;
+			for (j=0; j<3; ++j){				// calculate A 
+				if (answer[j] == guess[j]) correctA+=1;		
 			}
-			if (correctA!=3) {
-				cout << "很遗憾，你没有在规定次数内猜对。答案是 ";
-				for (j=0; j<3; ++j){				// output the correct answer 
-					cout << answer[j];
-				}
+			for (j=0; j<3; ++j){				// calculate B
+				if (answer[j]==guess[(j+1)%3] || answer[j]==guess[(j+2)%3])	correctB+=1;	
 			}
-			cout << endl << "需要玩游戏吗（Y/N）？ ";		// ask the player if he wants to continue
-			cin >> choose; 
-		} while (choose=='Y');
+			if (correctA==3){
+				win+=1;							// record innings for the win 
+				cout << "¹§Ï²£¬Äã²Â¶ÔÁË";
+				break; 
+			}
+			else cout << correctA << 'A' << correctB << 'B' << endl;	// output A,B
+		}
+		if (correctA!=3) {
+			cout << "ºÜÒÅº¶£¬ÄãÃ»ÓÐÔÚ¹æ¶¨´ÎÊýÄÚ²Â¶Ô¡£´ð°¸ÊÇ ";
+			for (j=0; j<3; ++j){				// output the correct answer 
+				cout << answer[j];
+			}
+		}
+		cout << endl << "ÐèÒªÍæÓÎÏ·Âð£¨Y/N£©£¿ ";		// ask the player if he wants to continue
+		cin >> choose; 
 	}
-	cout << "你一共玩了" << games << "局，赢了" << win 
-		 << "局，输了" << games-win << "局\n";		// output the record
+	cout << "ÄãÒ»¹²ÍæÁË" << games << "¾Ö£¬Ó®ÁË" << win 
+		 << "¾Ö£¬ÊäÁË" << games-win << "¾Ö\n";		// output the record
 	
 	return 0;
 }
